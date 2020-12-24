568c054e31fc7076ec41dcdc48e169a00da47916
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-12-24 13:37:30 +0530

    Working fine + 1 Extraa Copy

---
+++ src/App1.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+var NumberFormat = require('react-number-format');
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,EOS,ADA,LTC,XLM,NEO,ETH,XRP,BCH,IETH,IOT,AAC,1ST,ZT,ZRX,YOYOW,XUC&tsyms=USD')
+      .then(res => {
+        const cryptos = res.data;
+        console.log(cryptos);
+        this.setState({cryptos: cryptos});
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {Object.keys(this.state.cryptos).map((key) => (
+
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} /></span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
