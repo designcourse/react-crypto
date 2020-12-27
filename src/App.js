54c831c3a409dc767686e445c2ca786c17d5d6e8
SanketParshuramPatil <sanket.9006@gmail.com>
2020-12-27 21:39:58 +0530

    Removed Old Entries

--- src/App.js
+++ src/App.js
@@ -1,55 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
 import Navbar from './components/Navbar/Navbar.jsx'
 import FooterPage from './components/Footer/FooterPage.jsx'
 import FullData from './components/FullData/FullData.jsx'
 
-
-var NumberFormat = require('react-number-format');
-
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cryptos: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT,USDT,XRP,LTC,BCH,ADA,BNB,USDC,EOS&tsyms=USD')
-      .then(res => {
-        const cryptos = res.data;
-        console.log(cryptos);
-        this.setState({cryptos: cryptos});
-      })
-  }
-
   render() {
     return (
-
       <div className="App">
         <Navbar />
-
-              <FullData />
-
-        <br></br>
-        {Object.keys(this.state.cryptos).map((key) => (
-          <div id="crypto-container">
-            <span className="left">{key}</span>
-            <span className="right"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'₹ '} /></span>
-          </div>
-
-        ))}
-
-      <FooterPage />
-
+        <FullData />
+        <FooterPage />
       </div>
-      
     );
   }
 }
-
 export default App;
