781be727b57ace9d097c8ff135e427ed8501dab7
SanketParshuramPatil <sanket.9006@gmail.com>
2020-12-27 09:55:37 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ class App extends Component {
 
   componentDidMount() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=INR')
+=======
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+>>>>>>> parent of 0b9bee0... Added other cryptos
 =======
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
 >>>>>>> parent of 0b9bee0... Added other cryptos
